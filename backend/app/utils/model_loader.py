0b3c4297db90afe153689110a4341c58cfabe701
Piyumani K.V.P <99950210+IT21227868@users.noreply.github.com>
2025-03-04 19:06:46 +0530

    Revert "First Commit"

--- backend/app/utils/model_loader.py
+++
@@ -1,8 +0,0 @@
-import joblib
-import os
-
-def load_model(model_path: str):
-    if os.path.exists(model_path):
-        return joblib.load(model_path)
-    else:
-        raise FileNotFoundError(f"Model file not found at {model_path}")
